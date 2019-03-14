062c6771866898845f9764fddbbc2025bfe5dcf1
Albert Ortiz Guillem <albert.ortiz.guillem@a5s109pc13.fib.upc.es>
2019-03-14 19:37:00 +0100

    Adaptado a Python3

--- 02.1_Huffman_AlberOrtiz_MarcoTerral.py
+++ 02.1_Huffman_AlberOrtiz_MarcoTerral.py
@@ -100,7 +100,7 @@ Dado un mensaje hallar la tabla de frecuencia de los caracteres que lo componen
 
 def dic2list(dic):
 	l = []
-	for key, val in dic.iteritems():
+	for key, val in dic.items():
 		l.append([key,val])
 	return l
 
@@ -108,11 +108,11 @@ def dic2list(dic):
 def tablaFrecuencias(mensaje):
     tabla = {}
     for a in mensaje:
-		if(not a in tabla):
-			tabla[a] = 1
-		else:
-			tabla[a] += 1
-    return dic2list(tabla)
+        if(not a in tabla):
+            tabla[a] = 1
+        else:
+            tabla[a] += 1
+    return sorted(dic2list(tabla))
  
 mensaje='La heroica ciudad dormía la siesta. El viento Sur, caliente y perezoso, empujaba las nubes blanquecinas que se rasgaban al correr hacia el Norte. En las calles no había más ruido que el rumor estridente de los remolinos de polvo, trapos, pajas y papeles que iban de arroyo en arroyo, de acera en acera, de esquina en esquina revolando y persiguiéndose, como mariposas que se buscan y huyen y que el aire envuelve en sus pliegues invisibles. Cual turbas de pilluelos, aquellas migajas de la basura, aquellas sobras de todo se juntaban en un montón, parábanse como dormidas un momento y brincaban de nuevo sobresaltadas, dispersándose, trepando unas por las paredes hasta los cristales temblorosos de los faroles, otras hasta los carteles de papel mal pegado a las esquinas, y había pluma que llegaba a un tercer piso, y arenilla que se incrustaba para días, o para años, en la vidriera de un escaparate, agarrada a un plomo. Vetusta, la muy noble y leal ciudad, corte en lejano siglo, hacía la digestión del cocido y de la olla podrida, y descansaba oyendo entre sueños el monótono y familiar zumbido de la campana de coro, que retumbaba allá en lo alto de la esbeltatorre en la Santa Basílica. La torre de la catedral, poema romántico de piedra,delicado himno, de dulces líneas de belleza muda y perenne, era obra del siglo diez y seis, aunque antes comenzada, de estilo gótico, pero, cabe decir, moderado por uninstinto de prudencia y armonía que modificaba las vulgares exageraciones de estaarquitectura. La vista no se fatigaba contemplando horas y horas aquel índice depiedra que señalaba al cielo; no era una de esas torres cuya aguja se quiebra desutil, más flacas que esbeltas, amaneradas, como señoritas cursis que aprietandemasiado el corsé; era maciza sin perder nada de su espiritual grandeza, y hasta sussegundos corredores, elegante balaustrada, subía como fuerte castillo, lanzándosedesde allí en pirámide de ángulo gracioso, inimitable en sus medidas y proporciones.Como haz de músculos y nervios la piedra enroscándose en la piedra trepaba a la altura, haciendo equilibrios de acróbata en el aire; y como prodigio de juegosmalabares, en una punta de caliza se mantenía, cual imantada, una bola grande debronce dorado, y encima otra más pequenya, y sobre ésta una cruz de hierro que acababaen pararrayos.'
 print(tablaFrecuencias(mensaje))   
@@ -133,16 +133,16 @@ obtenido a partir de las frecuencias de los caracteres del mensaje.
 Definir otra función que decodifique los mensajes codificados con la función 
 anterior.
 '''
-
+'''
 def EncodeHuffman(mensaje_a_codificar):
 
     return mensaje_codificado, m2c
-    
+
     
 def DecodeHuffman(mensaje_codificado,m2c):
 
     return mensaje_decodificado
-        
+'''      
 
 """
 Ejemplo
